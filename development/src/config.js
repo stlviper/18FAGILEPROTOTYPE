a4fc0f17aaf41e9cd0602084c853f946c38394ec
Venu Duggireddy <venu.dvmr@gmail.com>
2015-07-06 16:20:46 -0400

    added apk

--- development/src/config.js
+++ development/src/config.js
@@ -1,3 +1,10 @@
+ /**
+ * author: venu duggireddy
+ * description: Config attributes for the application
+ * version:0.0.1
+ * Date:06/20/2015
+ */
+
 var config = {
   base_url:'https://api.fda.gov/drug/event.json?',
   api_key: 'hio1wf7pqYENmmnF8Q0THFA94muLUSMqwy1nzYo7',
@@ -5,25 +12,7 @@ var config = {
   drug_event_url:'https://api.fda.gov/drug/event.json?',
   drug_enforcement_url:'https://api.fda.gov/drug/enforcement.json?',
   device_enforcement_url:'https://api.fda.gov/device/enforcement.json?',
-  food_enforcement_url:'https://api.fda.gov/food/enforcement.json?',
-  environment:{
-    development:{
-        host:'http://localhost',
-        port:'80'
-    },
-    intergration:{
-        host:'http://10.63.48.27',
-        port:'80'
-    },
-    ci:{
-        host:'54.175.186.120',
-        port:'4000'
-    },
-    production:{
-      host:'http://54.85.23.143',
-      port:'80'
-    }
-  }
+  food_enforcement_url:'https://api.fda.gov/food/enforcement.json?'
 };
 
 module.exports = config;
